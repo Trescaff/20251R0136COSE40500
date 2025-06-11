5b4224388ebe48c9e00727f7b776bba11d32b70c
Trescaff <115195476+Trescaff@users.noreply.github.com>
2025-06-11 17:39:23 +0900

    Sixth

--- qr-code-app/backend/index.js
+++ qr-code-app/backend/index.js
@@ -18,5 +18,6 @@ mongoose.connect(process.env.MONGO_URI)
 app.use('/api/albums', albumRoutes);
 app.use('/api/photos', photoRoutes);
 
+// Listen on a specified port
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
